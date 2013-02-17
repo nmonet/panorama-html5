ec06058ac7ff6c22d587f29fe7c90d3e0eaf1fa0
nmonet <nmonet74@gmail.com>
2013-02-17 16:41:41 +0100

    Fix origin problem, bandeau noir a gauche

--- panorama-csstransform.js
+++ panorama-csstransform.js
@@ -325,6 +325,9 @@ window.AffichePanorama = (function (window, document, undefined) {
 	
 	AffichePanorama.goToOrigin = function () {
 		AffichePanorama.x = -(AffichePanorama.panorama.origin) * AffichePanorama.fov + $(window).width() / 2 ;
+		if (AffichePanorama.x >= 0) {
+				AffichePanorama.x = 0;
+		}
 		AffichePanorama.y = 0 ;
 	}
 	
